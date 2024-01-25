b870c263a4f0a0fd4b8e5fbdf8a9dff84926eaf0
Darren De Vouge <darrn.dv@gmail.com>
2024-01-25 17:38:58 -0500

    Inherited layouts

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,17 @@
-<div>Hello I'm a blade template</div>
+@extends('layouts.app')
 
-@isset($name)
-    <div>My name is: {{$name}}</div>
-@endisset
+<div> 
+@section('title', 'The list of tasks')
+
+@section('content')
+    {{-- @if (count($tasks)) --}} 
+        @forelse($tasks as $task)
+        <div>
+        <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+        </div>
+        @empty
+        <div>There are no tasks!</div>
+        @endforelse
+    {{-- endif --}}
+</div>
+@endsection
